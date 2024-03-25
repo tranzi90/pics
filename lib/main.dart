f986c4d06c6398f8927e2dee1cade6664243d568
denis_levchenko <tranzi90@gmail.com>
2024-03-26 01:32:18 +0300

    app => src

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   var app = MaterialApp(
-    home: Text('pics here'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Tap "+" to see images!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('clicked FAB');
+        },
+      ),
+    ),
   );
 
   runApp(app);
