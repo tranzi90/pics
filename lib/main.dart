09eaaa972480d930f7d0eecd853370eb7b9fd64e
denis_levchenko <tranzi90@gmail.com>
2024-03-27 02:43:51 +0300

    add image model

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Tap "+" to see images!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('clicked FAB');
-        },
-      ),
-    ),
-  );
-
-  runApp(app);
+  runApp(App());
 }
---
+++ lib/src/app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tap "+" to see images!'),
+        ),
+        body: Text('$counter'),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              counter++;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
