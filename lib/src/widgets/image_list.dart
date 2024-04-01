36a1389e3ca012f9c76eb4390da376d367536c2a
denis_levchenko <tranzi90@gmail.com>
2024-04-01 21:47:57 +0300

    add border & title

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -12,10 +12,23 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
         itemCount: images.length,
         itemBuilder: (context, int index) {
-          return Container(
-            margin: EdgeInsets.all(20.0),
-            child: Image.network(images[index].url),
-          );
+          return buildImage(images[index]);
         });
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(image.url),
+          Text(image.title),
+        ],
+      ),
+    );
   }
 }
